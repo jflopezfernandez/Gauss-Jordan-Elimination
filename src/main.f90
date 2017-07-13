225272a149d4d1f3236649424f6cf6910d4b6657
lopezfjose <jflopezfernandez@gmail.com>
2017-07-13 02:03:35 -0400

    Error during data input

--- src/main.f90
+++ src/main.f90
@@ -1,8 +1,55 @@
+! Author: Stephen J. Chapman
+! Fortran for Scientists and Engineers
 
 PROGRAM TEST
 IMPLICIT NONE
 
-  WRITE (*,*) 'TESTING...'
+  INTEGER, PARAMETER :: FILE_UNIT = 25
+
+  INTEGER, PARAMETER :: MAX_SIZE = 10
+  REAL, DIMENSION(MAX_SIZE,MAX_SIZE) :: A
+  REAL,DIMENSION(MAX_SIZE) :: B
+  INTEGER :: ERROR
+  CHARACTER(LEN=20) :: FILE_NAME
+  
+  INTEGER :: I
+  INTEGER :: J
+  INTEGER :: N
+  INTEGER :: ISTAT
+  
+  WRITE (*,"(' ENTER THE FILE NAME CONTAINING THE EQUATIONS: ')")
+  READ (*,'(A20)') FILE_NAME
+  
+  OPEN (UNIT=FILE_UNIT, FILE=FILE_NAME, STATUS='OLD', ACTION='READ', IOSTAT=ISTAT)
+  
+  FILEOPEN: IF (ISTAT == 0) THEN
+    READ (FILE_UNIT,*) N
+    
+    SIZE_OK: IF (N <= MAX_SIZE) THEN
+      DO I = 1, N
+        READ (FILE_UNIT,*) (A(I,J), J=1,N), B(I)
+      END DO
+      
+      CALL GAUSS_JORDAN_REDUCTION(A, B, MAX_SIZE, N, ERROR)
+      
+      ERROR_CHECK: IF (ERROR /= 0) THEN
+        WRITE (*,1010)
+        1010 FORMAT (/1X,'ZERO PIVOT ENCOUNTERED!',//1X, 'THERE IS NO UNIQUE SOLUTION TO THIS SYSTEM.')
+      ELSE ERROR_CHECK
+        WRITE (*,"(/,1X,'THE SOLUTIONS ARE:')")
+        
+        DO I = 1, N
+          WRITE (*,"(3X,'X(',I2,') = ',F16.6)") I, B(I)
+        END DO
+        
+      END IF ERROR_CHECK
+    END IF SIZE_OK
+  ELSE FILEOPEN
+    WRITE (*,1020) ISTAT
+    1020 FORMAT (1X,'FILE OPEN FAILED--STATUS = ', I6)
+  END IF FILEOPEN
+  
+  CLOSE (FILE_UNIT)
   
 END PROGRAM TEST
 
@@ -61,13 +108,13 @@ IMPLICIT NONE
     ! it to equation jrow (for all eqns except irow itself)
     ELIMINATE: DO JROW = 1, N
       IF (JROW /= IROW) THEN
-        FACTOR = -A(JROW,IROW)/A(IROW,IR0W)
+        FACTOR = -A(JROW,IROW)/A(IROW,IROW)
         
         DO KCOL = 1, N
           A(JROW,KCOL) = A(IROW,KCOL) * FACTOR + A(JROW,KCOL)
         END DO
         
-        B(JROW = B(IROW) * FACTOR + B(JROW)
+        B(JROW) = B(IROW) * FACTOR + B(JROW)
       END IF
     END DO ELIMINATE
     
