850f9a6015f3b08e4694fb93fac083e91615c051
lopezfjose <jflopezfernandez@gmail.com>
2017-07-13 01:42:41 -0400

    Subroutine complete

--- src/main.f90
+++ src/main.f90
@@ -7,7 +7,6 @@ IMPLICIT NONE
 END PROGRAM TEST
 
 
-
 SUBROUTINE GAUSS_JORDAN_REDUCTION(A, B, NDIM, N, ERROR)
 IMPLICIT NONE
 
@@ -40,7 +39,51 @@ IMPLICIT NONE
     END DO MAX_PIVOT
     
     ! Check for singular equations
-  
+    SINGULAR: IF (ABS(A(IPEAK,IROW)) < EPSILON) THEN
+      ERROR = 1
+      RETURN
+    END IF singular
+      
+    ! Otherwise, if ipeak /= irow, swap equations irow & ipeak
+    SWAP_EQUATION: IF (IPEAK /= IROW) THEN
+      DO KCOL = 1,N
+        TEMP = A(IPEAK, KCOL)
+        A(IPEAK,KCOL) = A(IROW,KCOL)
+        A(IROW,KCOL) = TEMP
+      END DO
+      
+      TEMP = B(IPEAK)
+      B(IPEAK) = B(IROW)
+      B(IROW) = TEMP
+    END IF SWAP_EQUATION
+    
+    ! Multiply equation irow by -a(jrow,irow)/a(irow,irow), and add
+    ! it to equation jrow (for all eqns except irow itself)
+    ELIMINATE: DO JROW = 1, N
+      IF (JROW /= IROW) THEN
+        FACTOR = -A(JROW,IROW)/A(IROW,IR0W)
+        
+        DO KCOL = 1, N
+          A(JROW,KCOL) = A(IROW,KCOL) * FACTOR + A(JROW,KCOL)
+        END DO
+        
+        B(JROW = B(IROW) * FACTOR + B(JROW)
+      END IF
+    END DO ELIMINATE
+    
   END DO MAIN_LOOP
+  
+  
+  ! End of mainloop over all equations. All off-diagonal terms are now zero. To
+  ! the final answer, we must divide each equation by the coefficient of its on-diagonal
+  ! term.
+  
+  DIVIDE: DO IROW = 1, N
+    B(IROW) = B(IROW) / A(IROW,IROW)
+    A(IROW,IROW) = 1.
+  END DO DIVIDE
+  
+  ! Set error flag to zero and return
+  ERROR = 0
   
 END SUBROUTINE GAUSS_JORDAN_REDUCTION
